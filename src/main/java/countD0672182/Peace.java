99ccf71b5f3422e942c3d275dcb294c9da4b6876
PC234 <d0672182@fcu.edu.tw>
2017-12-06 12:08:59 +0800

    11111

---
+++ src/main/java/countD0672182/Peace.java
@@ -0,0 +1,9 @@
+package countD0672182;
+
+
+
+public class Peace {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/countD0672182/TriangleTest.java
@@ -0,0 +1,14 @@
+package countD0672182;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+    @Test
+    public void getType() throws Exception {
+
+
+    }
+
+}
