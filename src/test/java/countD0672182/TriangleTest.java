e867eb2311f4b2711f572d8f10925bb8af0b2306
PC234 <d0672182@fcu.edu.tw>
2017-12-06 12:26:03 +0800

    111

--- src/test/java/countD0672182/TriangleTest.java
+++ src/test/java/countD0672182/TriangleTest.java
@@ -1,14 +1,27 @@
 package countD0672182;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import java.util.Arrays;
 
 public class TriangleTest {
     @Test
-    public void getType() throws Exception {
+    private final double[] e;
 
+    public TriangleTest(double a, double b, double c) {
+        this.e = new double[]{a, b, c};
+        Arrays.sort(this.e);
+    }
 
+    public String getType() {
+        if (e[0] + e[1] <= e[2]) {
+            return "不是三角形";
+        }
+        if (e[0] * e[0] + e[1] * e[1] > e[2] + e[2]) {
+            return "銳角三角形";
+        }
+        if (e[0] * e[0] + e[1] * e[1] == e[2] + e[2]) {
+            return "直角三角形";
+        }
+        return "鈍角三角形";
     }
 
 }
